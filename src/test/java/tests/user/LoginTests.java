ba17f59346478cdde22d3f744fd0cc0ffb5d2073
Natali-QA-ooops <80644500+Natali-QA-ooops@users.noreply.github.com>
2022-04-10 19:20:06 +0300

    chenge login class

--- src/test/java/tests/user/LoginTests.java
+++ src/test/java/tests/user/LoginTests.java
@@ -30,18 +30,18 @@ public class LoginTests extends TestBase {
         homePage.logOut();
     }
 
-    //Registered user can't login with wrong email and valid password
-    @Test  (dataProvider = "loginWrongEmailDataCSV", dataProviderClass = MyDataProvider.class)
-    public void wrongEmailValidPassTest(String email, String password) {
-        homePage.openLoginRegForm();
-        Assert.assertTrue(loginAndRegisterPage.loginFormIsDisplayed());
-        loginAndRegisterPage.fillLoginForm(email,password);
-        loginAndRegisterPage.clickBTNLogIn();
-
-        Assert.assertTrue(loginAndRegisterPage.allertIsDisplayed());    //FORM Oops! We found some errors - please check the error messages below and try again
-       // Assert.assertTrue(loginAndRegisterPage.massageWrongFormatIsDisplayed());
-        Assert.assertTrue(loginAndRegisterPage.massageErrorIsDisplaeyd()); ////TEXT Oops! We found some errors - please check the error messages below and try again
-    }
+//    //Registered user can't login with wrong email and valid password
+//    @Test  (dataProvider = "loginWrongEmailDataCSV", dataProviderClass = MyDataProvider.class)
+//    public void wrongEmailValidPassTest(String email, String password) {
+//        homePage.openLoginRegForm();
+//        Assert.assertTrue(loginAndRegisterPage.loginFormIsDisplayed());
+//        loginAndRegisterPage.fillLoginForm(email,password);
+//        loginAndRegisterPage.clickBTNLogIn();
+//
+//        Assert.assertTrue(loginAndRegisterPage.allertIsDisplayed());    //FORM Oops! We found some errors - please check the error messages below and try again
+//       // Assert.assertTrue(loginAndRegisterPage.massageWrongFormatIsDisplayed());
+//        Assert.assertTrue(loginAndRegisterPage.massageErrorIsDisplaeyd()); ////TEXT Oops! We found some errors - please check the error messages below and try again
+//    }
 //
 //
 //    @Test   //Registered user can't login with incorrect password and valid email
